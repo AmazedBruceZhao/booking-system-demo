4ff32d967aa76d8056502c2bd7ba91860880c3b9
AmazedBruceZhao <zrz0015@gmail.com>
2018-08-22 18:13:03 +0800

    calendar component updated

--- src/container/calendar.js
+++ src/container/calendar.js
@@ -1,16 +1,113 @@
 import React, { Component } from 'react';
-import Header from "../component/header";
-import soap from '../lib/soap'
+import dateFns from 'date-fns'
 import xml2js from 'xml2js'
-import Day from "./day";
+import soap from '../lib/soap'
 
 class Calendar extends Component {
     constructor(props){
         super(props)
         this.state = {
-            meetings : []
+            meetings : [],
+            currentMonth: new Date(),
+            selectedDate: new Date()
         }
     }
+    renderHeader() {
+        const dateFormat = "MMMM YYYY";
+        return (
+            <div className="header row flex-middle">
+                <div className="col col-start">
+                    <div className="icon" onClick={this.prevMonth}>
+                        chevron_left
+                    </div>
+                </div>
+                <div className="col col-center">
+                    <span>
+                      {dateFns.format(this.state.currentMonth, dateFormat)}
+                    </span>
+                </div>
+                <div className="col col-end" onClick={this.nextMonth}>
+                    <div className="icon">chevron_right</div>
+                </div>
+            </div>
+        );
+    }
+
+    renderDays() {
+        const dateFormat = "dddd";
+        const days = [];
+        let startDate = dateFns.startOfWeek(this.state.currentMonth);
+        for (let i = 0; i < 7; i++) {
+            days.push(
+                <div className="col col-center" key={i}>
+                    {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+                </div>
+            );
+        }
+        return <div className="days row">{days}</div>;
+    }
+
+    renderCells() {
+        const {currentMonth, selectedDate} = this.state;
+        const monthStart = dateFns.startOfMonth(currentMonth);
+        const monthEnd = dateFns.endOfMonth(monthStart);
+        const startDate = dateFns.startOfWeek(monthStart);
+        const endDate = dateFns.endOfWeek(monthEnd);
+
+        const dateFormat = "D";
+        const rows = [];
+
+        let days = [];
+        let day = startDate;
+        let formattedDate = "";
+
+        while (day <= endDate) {
+            for (let i = 0; i < 7; i++) {
+                formattedDate = dateFns.format(day, dateFormat);
+                const cloneDay = day;
+                days.push(
+                    <div
+                        className={`col cell ${
+                            !dateFns.isSameMonth(day, monthStart)
+                                ? "disabled"
+                                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
+                            }`}
+                        key={day}
+                        onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+                    >
+                        <span className="number">{formattedDate}</span>
+                        <span className="bg">{formattedDate}</span>
+                    </div>
+                );
+                day = dateFns.addDays(day, 1);
+            }
+            rows.push(
+                <div className="row" key={day}>
+                    {days}
+                </div>
+            );
+            days = [];
+        }
+        return <div className="body">{rows}</div>;
+    }
+
+    onDateClick = day => {
+        this.setState({
+            selectedDate: day
+        });
+    };
+
+    nextMonth = () => {
+        this.setState({
+            currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+        });
+    };
+
+    prevMonth = () => {
+        this.setState({
+            currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+        });
+    };
 
     render(){
         console.log(this.state.meetings)
@@ -25,8 +122,9 @@ class Calendar extends Component {
         })
         return (
             <div>
-                <Header/>
-                {test}
+                {this.renderHeader()}
+                {this.renderDays()}
+                {this.renderCells()}
             </div>
         )
     }
