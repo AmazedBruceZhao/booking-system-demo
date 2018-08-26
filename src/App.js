d9b8c2cd9bf74d2d9a5adf1f8bb4a0a24844e9fb
AmazedBruceZhao <zrz0015@gmail.com>
2018-08-26 19:02:04 +0800

    finish meeting detail

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Calendar from "./container/calendar";
+import { Header, Container } from 'semantic-ui-react'
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="App-intro">
+      <div>
+        <Header>
+          <h1>Booking System</h1>
+        </Header>
+        <Container>
           <Calendar/>
-        </div>
+        </Container>
       </div>
     );
   }
--- src/container/calendar.js
+++ src/container/calendar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import dateFns from 'date-fns'
 import xml2js from 'xml2js'
 import soap from '../lib/soap'
+import { Grid, Segment, Label, Button } from 'semantic-ui-react'
+import MeetingDetails from '../component/meetingDetails'
 
 class Calendar extends Component {
     constructor(props){
@@ -15,21 +17,21 @@ class Calendar extends Component {
     renderHeader() {
         const dateFormat = "MMMM YYYY";
         return (
-            <div className="header row flex-middle">
-                <div className="col col-start">
+            <Grid.Row columns={3}>
+                <Grid.Column textAlign='left'>
                     <div className="icon" onClick={this.prevMonth}>
                         chevron_left
                     </div>
-                </div>
-                <div className="col col-center">
+                </Grid.Column>
+                <Grid.Column textAlign='center'>
                     <span>
                       {dateFns.format(this.state.currentMonth, dateFormat)}
                     </span>
-                </div>
-                <div className="col col-end" onClick={this.nextMonth}>
-                    <div className="icon">chevron_right</div>
-                </div>
-            </div>
+                </Grid.Column >
+                <Grid.Column textAlign='right' onClick={this.nextMonth} >
+                    <div>chevron_right</div>
+                </Grid.Column>
+            </Grid.Row>
         );
     }
 
@@ -39,12 +41,12 @@ class Calendar extends Component {
         let startDate = dateFns.startOfWeek(this.state.currentMonth);
         for (let i = 0; i < 7; i++) {
             days.push(
-                <div className="col col-center" key={i}>
+                <Grid.Column key={i}>
                     {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
-                </div>
+                </Grid.Column>
             );
         }
-        return <div className="days row">{days}</div>;
+        return <Grid.Row columns={7} >{days}</Grid.Row>;
     }
 
     renderCells() {
@@ -61,34 +63,37 @@ class Calendar extends Component {
         let day = startDate;
         let formattedDate = "";
 
+
         while (day <= endDate) {
             for (let i = 0; i < 7; i++) {
                 formattedDate = dateFns.format(day, dateFormat);
+
                 const cloneDay = day;
+                const currentDate = dateFns.format(day, 'YYYYMMDD')
                 days.push(
-                    <div
-                        className={`col cell ${
-                            !dateFns.isSameMonth(day, monthStart)
-                                ? "disabled"
-                                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
-                            }`}
-                        key={day}
-                        onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
-                    >
-                        <span className="number">{formattedDate}</span>
-                        <span className="bg">{formattedDate}</span>
-                    </div>
+                    <Grid.Column key={day}>
+                        <Segment style={{height: 100}}
+                            disabled={!dateFns.isSameMonth(day, monthStart)}
+                            color={dateFns.isSameDay(day, selectedDate) ? 'blue' : 'grey'}
+                            onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+                        ><Label as='a' color='blue' ribbon>
+                            {formattedDate}
+                        </Label>
+                            <Button icon='add' size='mini' floated='right'/>
+                            <MeetingDetails data={currentDate in this.state.meetings ? this.state.meetings[currentDate] : []}/>
+                        </Segment>
+                    </Grid.Column>
                 );
                 day = dateFns.addDays(day, 1);
             }
             rows.push(
-                <div className="row" key={day}>
+                <Grid.Row columns={7} key={day}>
                     {days}
-                </div>
+                </Grid.Row>
             );
             days = [];
         }
-        return <div className="body">{rows}</div>;
+        return <Grid>{rows}</Grid>;
     }
 
     onDateClick = day => {
@@ -110,27 +115,26 @@ class Calendar extends Component {
     };
 
     render(){
-        console.log(this.state.meetings)
-        const test = this.state.meetings.map((meeting, key) => {
 
-            return (
-                <div key={key}>
-                    <Day data={meeting} />
-                </div>
-            )
-
-        })
         return (
-            <div>
+            <Grid>
                 {this.renderHeader()}
                 {this.renderDays()}
                 {this.renderCells()}
-            </div>
+            </Grid>
         )
     }
 
-    componentDidMount(){
-        soap('<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"\n' +
+    updateMeetings(){
+        const {currentMonth} = this.state;
+        const monthStart = dateFns.startOfMonth(currentMonth);
+        const monthEnd = dateFns.endOfMonth(monthStart);
+        const startDate = dateFns.startOfWeek(monthStart);
+        const endDate = dateFns.endOfWeek(monthEnd);
+        const start = dateFns.format(dateFns.subHours(startDate, 8), 'YYYY-MM-DD[T]HH:MM:ss[Z]');
+        const end = dateFns.format(dateFns.subHours(endDate, 8), 'YYYY-MM-DD[T]HH:MM:ss[Z]');
+
+        const query = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"\n' +
             '\txmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"\n' +
             '\txmlns:m="http://schemas.microsoft.com/exchange/services/2006/messages"\n' +
             '\txmlns:t="http://schemas.microsoft.com/exchange/services/2006/types">\n' +
@@ -139,48 +143,75 @@ class Calendar extends Component {
             '      <m:ItemShape>\n' +
             '        <t:BaseShape>Default</t:BaseShape>\n' +
             '      </m:ItemShape>\n' +
-            '      <m:CalendarView MaxEntriesReturned="100" StartDate="2017-09-09T12:00:00Z" EndDate="2018-09-13T12:00:00Z" />\n' +
+            `      <m:CalendarView MaxEntriesReturned="100" StartDate="${start}" EndDate="${end}" />\n` +
             '      <m:ParentFolderIds>\n' +
             '        <t:DistinguishedFolderId Id="calendar"/>\n' +
             '      </m:ParentFolderIds>\n' +
             '    </m:FindItem>\n' +
             '</soap:Body>\n' +
-            '</soap:Envelope>').then(res => {
-                return res.text()
-            }).then(data => {
-                let that = this
-                xml2js.parseString(data, function (err, result) {
-                    if (!err){
-                        //console.log(result);
-                        let res = result['s:Envelope']["s:Body"][0]["m:FindItemResponse"][0]["m:ResponseMessages"][0]["m:FindItemResponseMessage"][0]
-                        let responseClass = res["$"]["ResponseClass"]
-                        let responseCode = res["m:ResponseCode"]
-                        if (responseClass === 'Success'){
-                            let meetings = res["m:RootFolder"][0]["t:Items"][0]["t:CalendarItem"].map((item) => {
-                                let start = item["t:Start"][0]
-                                let end = item["t:End"][0]
+            '</soap:Envelope>';
+
+        soap(query).then(res => {
+            return res.text()
+        }).then(data => {
+            let that = this
+            xml2js.parseString(data, function (err, result) {
+                if (!err){
+                    //console.log(result);
+                    let res = result['s:Envelope']["s:Body"][0]["m:FindItemResponse"][0]["m:ResponseMessages"][0]["m:FindItemResponseMessage"][0]
+                    let responseClass = res["$"]["ResponseClass"]
+                    let responseCode = res["m:ResponseCode"]
+                    if (responseClass === 'Success'){
+                        const meetings = []
+                        if(res["m:RootFolder"][0]["t:Items"][0]["t:CalendarItem"]){
+                            res["m:RootFolder"][0]["t:Items"][0]["t:CalendarItem"].map((item) => {
+                                let start = dateFns.format(dateFns.parse(item["t:Start"][0]), 'HH:MM:ss')
+                                let end = dateFns.format(dateFns.parse(item["t:End"][0]), 'HH:MM:ss')
                                 let location = item["t:Location"] ? item["t:Location"][0] : ''
                                 let subject = item["t:Subject"][0]
                                 let organizers = item["t:Organizer"].map((item) => {
                                     return item["t:Mailbox"][0]["t:Name"]
                                 })
-                                return {
+                                const object = {
                                     start: start,
                                     end: end,
                                     location: location,
                                     subject: subject,
                                     organizers: organizers
                                 }
-                            })
-                            //console.log(meetings)
-                            that.setState({
-                                meetings: meetings
+                                let date = dateFns.format(dateFns.parse(item["t:Start"][0]), 'YYYYMMDD')
+                                if(date in meetings){
+                                    meetings[date].push(
+                                        object
+                                    )
+                                }else{
+                                    meetings[date] = [object]
+                                }
+
                             })
                         }
+                        //console.log(meetings)
+                        that.setState({
+                            meetings: meetings
+                        })
                     }
-                });
-            })
+                }
+            });
+        })
     }
+
+    componentDidMount(){
+        this.updateMeetings();
+        this.intervalId = setInterval(() => {
+            this.updateMeetings();
+        }, 2000);
+    }
+
+    componentWillUnMount(){
+        clearInterval(this.intervalId)
+    }
+
+
 }
 
 export default Calendar
